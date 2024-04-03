517099004b8c4ba6a3fa7e4068723b229099d941
UMMANCODE <ummanmm@code.edu.az>
2024-04-04 03:20:06 +0400

    Files added

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:seventh_project/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:seventh_project/screens/chat.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: ".env");
   runApp(const App());
 }
 
